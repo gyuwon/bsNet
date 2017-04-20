4d9cf42fd8ceb3262aa55c493bad66e028fd6d46
hyej55 <hyej@bsidesoft.com>
2017-04-20 16:01:35 +0900

    hyej

--- XUnitTestProject1/bsVali.cs
+++ XUnitTestProject1/bsVali.cs
@@ -12,7 +12,8 @@ namespace test
     public class bsVali
     {
         private bs bs;
-        public bsVali() {
+        public bsVali()
+        {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("C:\\Users\\hika0\\Documents\\Visual Studio 2017\\Projects\\WebApplication2\\WebApplication2\\appsettings.json", optional: false, reloadOnChange: true)
@@ -20,22 +21,65 @@ namespace test
             var bs = new bs(null);
             bs.service(builder.Build(), null);
         }
+        /*
+        [Fact]
+        public void Test0() {
+            bs.vali("test0", "a", "creditCard", "b", "html", "c", "image");
+            var result = bs.valiResult();
+            var isOK = bs.vali("test0").check(out result, "a", "5342920001495006", "b", "<div>div<span>span</span></div><br>", "c", "asdfasdf.jpg");
+            Assert.Equal(isOK, true);
+            Assert.Equal(result["a"].value, "5342920001495006");
+            Assert.Equal(result["b"].value, "<div>div<span>span</span></div><br>");
+            Assert.Equal(result["c"].value, "asdfasdf.jpg");
+        }
         [Fact]
         public void Test1()
         {
-            bs.vali("test1", "a", "int", "b", "float");
+            bs.vali("test1", "a", "ip", "b", "url", "c", "email");
             var result = bs.valiResult();
-            var isOK = bs.vali("test1").check(out result, "a", "1242", "b", "33.3452");
+            var isOK = bs.vali("test1").check(out result, "a", "112.220.245.82", "b", "http://www.naver.com", "c", "hyej.kim@bsidesoft.com");
             Assert.Equal(isOK, true);
-            Assert.Equal(result["a"].value, 1242);
-            Assert.Equal(result["b"].value, 33.3452F);
+            Assert.Equal(result["a"].value, "112.220.245.82");
+            Assert.Equal(result["b"].value, "http://www.naver.com");
+            Assert.Equal(result["c"].value, "hyej.kim@bsidesoft.com");
         }
         [Fact]
         public void Test2()
         {
-            bs.vali("test2", "a", "ip", "b", "url", "c", "url");
+            bs.vali("test2", "a", "korean", "b", "japanese");
             var result = bs.valiResult();
-            var isOK = bs.vali("test1").check(out result, "a", "1242", "b", "33.3452");
+            var isOK = bs.vali("test2").check(out result, "a", "¾È³ç", "b", "ª¢ªêª¬ªÈª¦ª´ª¶ª¤ªÞª¹");
+            Assert.Equal(isOK, true);
+            Assert.Equal(result["a"].value, "¾È³ç");
+            Assert.Equal(result["b"].value, "ª¢ªêª¬ªÈª¦ª´ª¶ª¤ªÞª¹");
+        }
+        [Fact]
+        public void Test3()
+        {
+            bs.vali("test3", "a", "alpha", "b", "ALPHA", "c", "alpha", "d", "1ALPHA");//|1alpha
+            var result = bs.valiResult();
+            var isOK = bs.vali("test3").check(out result, "a", "abcdefghijklmnopqrstuvwxyz", "b", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "c", "a", "d", "Y");
+            Assert.Equal(isOK, true);
+            Assert.Equal(result["a"].value, "abcdefghijklmnopqrstuvwxyz");
+            Assert.Equal(result["b"].value, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+            Assert.Equal(result["c"].value, "a");
+            Assert.Equal(result["d"].value, "Y");
+        }
+        [Fact]
+        public void Test4()
+        {
+            bs.vali("test4", "a", "num", "b", "alphanum");
+            var result = bs.valiResult();
+            var isOK = bs.vali("test4").check(out result, "a", "-1234.567890", "b", "abcdefghijklmnopqrstuvwxyz0123456789");
+            Assert.Equal(isOK, true);
+            Assert.Equal(result["a"].value, "-1234.567890");
+            Assert.Equal(result["b"].value, "abcdefghijklmnopqrstuvwxyz0123456789");
+        }*/
+        [Fact]
+        public void Test10() {
+            bs.vali("test10", "a", "int", "b", "float");
+            var result = bs.valiResult();
+            var isOK = bs.vali("test10").check(out result, "a", "1242", "b", "33.3452");
             Assert.Equal(isOK, true);
             Assert.Equal(result["a"].value, 1242);
             Assert.Equal(result["b"].value, 33.3452F);
--- bs/bs/validator/Rule.cs
+++ bs/bs/validator/Rule.cs
@@ -12,12 +12,12 @@ namespace com.bsidesoft.cs {
                 add("html", new RuleVali(new DataTypeAttribute(DataType.Html)));
                 add("image", new RuleVali(new DataTypeAttribute(DataType.ImageUrl)));
                 //base
-                add("ip", new RuleVali(new RegularExpressionAttribute("^((([0-9])|(1[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))[.]){3}(([0-9])|(1[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))$")));
+                add("ip", new RuleVali(new RegularExpressionAttribute("^((([0-9])|(1[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))[.]){3}(([0-9])|([1-9][0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))$")));
                 add("url", new RuleVali(new DataTypeAttribute(DataType.Url)));
                 add("email", new RuleVali(new DataTypeAttribute(DataType.EmailAddress)));
                 add("korean", new RuleVali(new RegularExpressionAttribute("^[ㄱ-힣]+$")));
                 add("japanese", new RuleVali(new RegularExpressionAttribute("^[ぁ-んァ-ヶー一-龠！-ﾟ・～「」“”‘’｛｝〜−]+$")));
-                add("alpha", new RuleVali(new RegularExpressionAttribute("^[a - z] +$")));
+                add("alpha", new RuleVali(new RegularExpressionAttribute("^[a-z]+$")));
                 add("ALPHA", new RuleVali(new RegularExpressionAttribute("^[A-Z]+$")));
                 add("num", new RuleVali(new RegularExpressionAttribute("^-?[0-9.]+$")));
                 add("alphanum", new RuleVali(new RegularExpressionAttribute("^[a-z0-9]+$")));
@@ -26,6 +26,30 @@ namespace com.bsidesoft.cs {
                 //type
                 add("equalto", new Rule((value, arg, safe) => {
                     return value == safe[arg[0]] ? value : FAIL;
+                }));
+                add("max", new Rule((value, arg, safe) => {
+                    if(value is string) {
+                        return ((string)value).Length < toI(arg[0]) ? value : FAIL;
+                    }
+                    return FAIL;
+                }));
+                add("min", new Rule((value, arg, safe) => {
+                    if(value is string) {
+                        return ((string)value).Length > toI(arg[0]) ? value : FAIL;
+                    }
+                    return FAIL;
+                }));
+                add("length", new Rule((value, arg, safe) => {
+                    if(value is string) {
+                        return ((string)value).Length == toI(arg[0]) ? value : FAIL;
+                    }
+                    return FAIL;
+                }));
+                add("range", new Rule((value, arg, safe) => {
+                    if(value is string) {
+                        return toI(arg[0]) <= ((string)value).Length && ((string)value).Length <= toI(arg[1]) ? value : FAIL;
+                    }
+                    return FAIL;
                 }));
                 add("int", new Rule((value, arg, safe) => {
                     if (value is string)
