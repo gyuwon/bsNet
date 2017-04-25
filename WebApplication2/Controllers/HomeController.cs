3ece1f214320ecfbe35df9797ce11b8ff5f5e23d
hika <hika@bsidesoft.com>
2017-04-25 18:08:07 +0900

    msg처리 추가

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -9,18 +9,11 @@ namespace WebApplication2.Controllers {
             bs = b;
         }
         public string _Index(ActionExecutingContext c) {
-            //bs.log(bs.to<string>("aaaa"));
-            bs.log((bs.to<int>("1234") is int)+ "");
-            //bs.log(bs.to<float>(333.1F) + "");
-            //bs.log(bs.to<double>(333.3) + "");
-            //bs.log(bs.to<bool>(false) + "");
-            
             return "test";
         }
         public IActionResult Index() {
             var r = bs.valiResult();
             var rs = bs.dbSelect(out r, "remote:a", "title", "1PD시험a");
-
             return Json(new { data = rs, a = bs.before(this)});
         }
     }
--- bs/bs/validator/Msg.cs
+++ bs/bs/validator/Msg.cs
@@ -1,23 +1,18 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 
 namespace com.bsidesoft.cs {
     public partial class bs {
         public class Msg {
-            private static ConcurrentDictionary<string, Msg> MSGS = new ConcurrentDictionary<string, Msg>();
-            public static Msg get(string key) {
-                Msg m;
-                if(!MSGS.TryGetValue(key, out m)) {
-                    log("Msg.get:fail to get - " + key);
-                    return null;
-                }
-                return m;
+            internal static ConcurrentDictionary<string, Msg> MSGS = new ConcurrentDictionary<string, Msg>();
+            private static Func<object, string, string[], Dictionary<string, object>, string> F = (v, r, arg, safe) => "error";
+            internal Msg(Func<object, string, string[], Dictionary<string, object>, string> func) {
+                f = func;
             }
-            public static void add(string key, Msg m) {
-                if(!MSGS.TryAdd(key, m)) log("Msg.add:fail to add - " + key);
-            }
+            private Func<object, string, string[], Dictionary<string, object>, string> f = F;
             virtual public string msg(object value, string rule, string[] arg, Dictionary<string, object> safe) {
-                return "error";
+                return f(value, rule, arg, safe);
             }
         }
     }
