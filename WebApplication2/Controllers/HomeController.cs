e845476188fae703e1ba8d911a3917711dabc00b
hika <hika@bsidesoft.com>
2017-05-01 11:36:09 +0900

    rsa, file

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -10,11 +10,11 @@ namespace WebApplication2.Controllers {
             bs = b;
         }
         public string _Index(ActionExecutingContext c) {
-            
             var a = JObject.Parse("{}");
             return "test";
         }
-        public IActionResult Index() {
+        public IActionResult Index(int id) {
+            
             var r = bs.valiResult();
             var rs = bs.dbSelect(out r, "remote:a", "title", "1PD시험a");
             return Json(new { data = rs, a = bs.before(this), b = bs.fr<string>(true, "test.html")});
---
+++ bs/bs/utils/_crypto.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace com.bsidesoft.cs {
+    public partial class bs {
+        private static UnicodeEncoding byteConverter = new UnicodeEncoding();
+        private static RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(1024);
+        private static RSAEncryptionPadding padding = RSAEncryptionPadding.Pkcs1;
+
+        static public string rsaEncrypt(string v, RSAParameters key, bool padding) {
+            try {
+                rsa.ImportParameters(key);
+                return byteConverter.GetString(rsa.Encrypt(byteConverter.GetBytes(v), padding));
+            } catch(CryptographicException e) {
+                return e + "";
+            }
+        }
+        static public string rsaDecrypt(string v, RSAParameters key, bool padding) {
+            try {
+                rsa.ImportParameters(key);
+                return byteConverter.GetString(rsa.Decrypt(byteConverter.GetBytes(v), padding));
+            } catch(CryptographicException e) {
+                return null;
+            }
+        }
+    }
+}
