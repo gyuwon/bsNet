92a248d12c933ae94f1a4c6c1c86e2b06ac01eac
Isaac G <jidolstar@cookilab.com>
2017-05-01 15:27:00 +0900

    교수 등록 관련 구현해봄. 아직 보완해야 함

--- WebApplication2/Startup.cs
+++ WebApplication2/Startup.cs
@@ -35,6 +35,11 @@ namespace WebApplication2 {
                     template: "professor/contents/{controller=Contents}/{action=Index}"
                 );
                 routes.MapRoute(
+                    name: "c",
+                    template: "professor/{action=Index}",
+                    defaults: new { controller = "Professor" }
+                );
+                routes.MapRoute(
                     name: "b",
                     template: "{action=Index}",
                     defaults: new { controller = "Home" }
